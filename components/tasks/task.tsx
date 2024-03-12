fcdb8a04ff74cf77fa40ba02d0a04f63a8cbe3b1
antonpalermo <anpbusinessmail@gmail.com>
2024-03-12 21:02:05 +0800

    mark task complete

--- components/tasks/task.tsx
+++ components/tasks/task.tsx
@@ -2,17 +2,48 @@
 
 import { Task as _Task } from "@prisma/client";
 
-import Menu from "./menu";
+import { Checkbox } from "@/components/ui/checkbox";
+
+import Menu from "@/components/tasks/menu";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 export type TaskProps = {
   task: _Task;
 };
 
 export default function Task({ task }: TaskProps) {
+  const router = useRouter();
+  const [checked, setChecked] = useState(task.isComplete);
+
+  async function handleChecked() {
+    try {
+      const request = await fetch(`/api/tasks/${task.id}`, {
+        method: "PATCH",
+        body: JSON.stringify({ complete: true })
+      });
+
+      if (!request.ok) {
+        // TODO: use toast here indicating that the requet failed.
+      }
+
+      setChecked(prev => !prev);
+    } catch (error) {
+      setChecked(false);
+      console.log(error);
+    }
+
+    router.refresh();
+  }
+
   return (
     <div className="border rounded px-3">
       <div className="py-2 flex flex-row items-center justify-between w-full">
-        <h2>{task.name}</h2>
+        <div className="inline-flex items-center justify-start space-x-3">
+          <Checkbox checked={checked} onCheckedChange={() => handleChecked()} />
+          <h2 className={cn(checked ? "line-through " : "")}>{task.name}</h2>
+        </div>
         <Menu task={task} />
       </div>
     </div>
